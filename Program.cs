ad70930bf2401ea05b9bbb6e1e5a03c6a066ffb1
alfarid04 <61859331+alfarid04@users.noreply.github.com>
2020-06-13 20:00:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.Nim = "2841";
+            mhs.Nama = "Alfarid P. Yusuf";
+            mhs.Registrasi();
+
+            Console.ReadKey();
+        }
+    }
+}
